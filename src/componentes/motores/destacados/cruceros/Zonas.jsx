9563d26ea7933eea6818a010cd2c7c06451dd3c9
Ana <probablynotannie@gmail.com>
2025-02-03 11:04:27 +0100

    Antes de romper algo.. no esnerio

--- src/componentes/motores/destacados/cruceros/Zonas.jsx
+++ src/componentes/motores/destacados/cruceros/Zonas.jsx
@@ -1,46 +1,18 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import zonas from "./jsons/zonas.json";
-import { useNavigate } from "react-router-dom";
-
-// Function to fetch data from the API
-const fetchCrucerosData = async (idZona) => {
-  const url = new URL(
-    "https://devxml-2.vpackage.net/FrontCruceros/searchjson?json=1"
-  );
-  url.searchParams.append("idZona", idZona); // Add the idZona to the query parameters
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error("Error al obtener los cruceros.");
-  }
-  const data = await response.json();
-  return data;
-};
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 function Zonas({ setRequestData }) {
-  const navigate = useNavigate();
-  const [selectedZona, setSelectedZona] = useState(null);
-
-  // Fetch data for the selected zone using the useQuery hook
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["cruceros", selectedZona],
-    queryFn: () => fetchCrucerosData(selectedZona),
-    enabled: !!selectedZona, // Only run the query if a zona is selected
-    onSuccess: (data) => {
-      if (data && data.idZona === parseInt(selectedZona)) {
-        console.log("Producto encontrado:", data);
-      } else {
-        console.log("No se encontró un producto con idZona igual a", selectedZona);
-      }
-    },
-    onError: (err) => {
-      console.error("Error fetching data", err);
-    },
-  });
-
-  // Handler for zone click
-  const handleZoneClick = (zona) => {
-    setSelectedZona(zona.val); // Set selectedZona to trigger the query
+  const navigate = useNavigate(); // Initialize navigation
+  const handleZoneClick = (producto) => {
+    const newRequestData = {
+      puerto: "",
+      destino: producto.val,
+      mes: "",
+      duracion: "",
+      naviera: "",
+    };
+    setRequestData(newRequestData);
+    navigate("/listadoCruceros", { state: { newRequestData, producto } });
   };
 
   return (
@@ -48,8 +20,6 @@ function Zonas({ setRequestData }) {
       <h2 className="tw-font-bold tw-text-xl xl:tw-text-2xl dark:tw-text-white">
         Zonas Destacados
       </h2>
-      {isLoading && <p>Cargando...</p>}
-      {error && <p className="tw-text-red-500">{error.message}</p>}
       <div className="tw-grid md:tw-grid-cols-2 xl:tw-grid-cols-1 tw-gap-3 tw-mt-3">
         {zonas.map((zona, index) => (
           <div key={index} onClick={() => handleZoneClick(zona)}>
