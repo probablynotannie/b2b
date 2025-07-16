07a3eb81d5046bfd90d94ee5e1f211a3ecf79a8d
Ana <probablynotannie@gmail.com>
2025-07-16 10:12:43 +0200

    Arreglo unos bordes en el itinerario de destino y detalles

--- src/componentes/reservas/productos/destinos/destino/Itinerario.jsx
+++ src/componentes/reservas/productos/destinos/destino/Itinerario.jsx
@@ -3,7 +3,7 @@ import { Timeline } from "flowbite-react";
 function Itinerario({ destino }) {
   return (
     <div className="tw-mt-5">
-      <Timeline>
+      <Timeline  className="tw-border-l-2 tw-border-slate-100 dark:tw-border-slate-700">
         {destino.itinerarioViaje.map((destino, index) => (
           <Timeline.Item key={index}>
             <Timeline.Point />
@@ -13,7 +13,7 @@ function Itinerario({ destino }) {
                   Día {destino.dia}
                 </span>
               </Timeline.Time>
-              <Timeline.Title className="tw-text-secondary dark:tw-text-white">
+              <Timeline.Title className="tw-text-secondary dark:tw-text-slate-300">
                 {destino.ciudad}, alojamiento en {destino.alojamiento}
               </Timeline.Title>
               <Timeline.Body>
