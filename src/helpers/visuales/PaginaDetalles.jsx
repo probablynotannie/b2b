31f0cd7ba50f29edab857d351237947fa184ae03
Ana <probablynotannie@gmail.com>
2025-07-21 14:46:10 +0200

    Vuelos+ hotel usa template de datos

--- src/helpers/visuales/PaginaDetalles.jsx
+++ src/helpers/visuales/PaginaDetalles.jsx
@@ -6,7 +6,7 @@ function PaginaDetalles({
   contenidoSecundario,
 }) {
   return (
-    <main className="tw-container lg:tw-grid lg:tw-grid-cols-3 tw-min-h-[55vh] tw-items-start tw-gap-y-10 tw-my-10 lg:tw-gap-12">
+    <main className="tw-container lg:tw-grid lg:tw-grid-cols-3 tw-min-h-[55vh] tw-items-start tw-space-y-10 tw-my-10 lg:tw-gap-12">
       <section className="tw-col-span-2 tw-shadow-lg hover:tw-shadow-xl tw-smooth tw-rounded-lg tw-min-h-[15vh] tw-border tw-border-slate-200 tw-bg-white dark:tw-border-slate-700 dark:tw-bg-slate-800 tw-p-5">
         <div className="tw-flex tw-justify-between tw-items-center tw-border-b-2 tw-border-slate-100 dark:tw-text-slate-200 dark:tw-border-slate-700/70 tw-pb-2">
           <div>
